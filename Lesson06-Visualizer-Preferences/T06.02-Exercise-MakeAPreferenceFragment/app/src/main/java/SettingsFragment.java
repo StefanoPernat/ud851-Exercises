d55c5319339d2efdf958f63cc9c19ff7f5ba835b
Stefano Pernat <stefano.pernat@gmail.com>
2017-02-20 19:00:11 +0100

    TODO 2: Created SettingFragment extends PreferenceFragmentCompat

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/SettingsFragment.java
@@ -0,0 +1,14 @@
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by stefanopernat on 20/02/17.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+}
