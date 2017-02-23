b15f95a6fc94b30645459c93b3e2487b0d51b2f4
Stefano Pernat <stefano.pernat@gmail.com>
2017-02-23 18:39:35 +0100

    TODO 4: Defined constructor that takes context as input and calls super

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,12 +1,15 @@
 package com.example.android.waitlist.data;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class WaitlistDbHelper extends SQLiteOpenHelper{
-    public static final String DATABASE_NAME = "waitlist.db";
-    public static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "waitlist.db";
+    private static final int DATABASE_VERSION = 1;
 
-    // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+    public WaitlistDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
     // TODO (5) Override the onCreate method
 
