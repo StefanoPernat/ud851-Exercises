8287178dc00d9626cb99616b86a81d7cb01c5ed8
Stefano Pernat <stefano.pernat@gmail.com>
2017-02-23 18:47:46 +0100

    TODO 5,6,7: Implemented onCreate

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,6 +1,7 @@
 package com.example.android.waitlist.data;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class WaitlistDbHelper extends SQLiteOpenHelper{
@@ -11,11 +12,17 @@ public class WaitlistDbHelper extends SQLiteOpenHelper{
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    // TODO (5) Override the onCreate method
-
-        // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
-
-        // TODO (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_WAITLIST_TABLE =
+                "CREATE TABLE " + WaitlistContract.WaitlistEntry.TABLE_NAME + " ("
+              + WaitlistContract.WaitlistEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+              + WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME + " TEXT NOT NULL,"
+              + WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE + " INTEGER NOT NULL,"
+              + WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+              + ");";
+        db.execSQL(SQL_CREATE_WAITLIST_TABLE);
+    }
 
     // TODO (8) Override the onUpgrade method
 
